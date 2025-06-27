c41232032909001259e0f3fb6b3ff98895e8b264
Hilixx <jhonalizan@gmail.com>
2025-06-27 19:18:54 -0300

    Minhas alterações locais

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
+import Logica.Logica;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+        Logica logica = new Logica();
+        System.out.println(logica.setPalavra());
     }
+
+
+
 }
